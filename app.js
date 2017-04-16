0b2223aebdf3128a801d08fcfe8aea47de335474
SangbumWoo <sbwoo87@gmail.com>
2017-04-16 21:01:56 +0900

    oil 및 종자정보 csv 추가 적용 수분함량계산 차트 추가

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
-var app = angular.module('myApp', ['ui-rangeSlider']);
+var app = angular.module('myApp', []);
 
 app.controller('MainController', function ($scope, $http, $q, $filter, filterFilter, $log) {
 
 
     $('#sandbox-container input').datepicker({});
 
-    $scope.varNames = {'':'-','subsp.':'subsp.','var.':'var.','for.':'for.','cv.':'cv.'};
+    $scope.varNames = {'': '-', 'subsp.': 'subsp.', 'var.': 'var.', 'for.': 'for.', 'cv.': 'cv.'};
 
-    $scope.dataTemp = {
-        "key1": "val1",
-        "key2": "val2",
-        "key3": "val3"
+    // $scope.dataTemp = {
+    //     "key1": "val1",
+    //     "key2": "val2",
+    //     "key3": "val3"
+    //
+    // };
 
-    };
     $scope.varNameChanged = function () {
         console.log('changed : ', $scope.data.name4);
         if (!$scope.data.name4) {
@@ -38,13 +39,28 @@ app.controller('MainController', function ($scope, $http, $q, $filter, filterFil
     //
     // }
 
+    // //Entandrophragma angolense,"Tompsett, 1992",4.6,2.21,0.033,0.000478
+    /*
+    "name1": "NO-NAME",
+    "name2": "Oryza",
+    "name3": "sativa",
+    "name4": "var.",
+    "name5": "japonica"
+
+     "name1": "",
+     "name2": "Entandrophragma",
+     "name3": "angolense",
+     "name4": "",
+     "name5": "",
+    */
+
 
     $scope.data = {
-        "name1": "NO-NAME",
-        "name2": "Oryza",
-        "name3": "sativa",
-        "name4": "var.",
-        "name5": "japonica",
+        "name1": "",
+        "name2": "Entandrophragma",
+        "name3": "angolense",
+        "name4": "",
+        "name5": "",
 
         "waterValue": 5.2,
         "inventoryCondition": -20,
@@ -63,14 +79,63 @@ app.controller('MainController', function ($scope, $http, $q, $filter, filterFil
     }
 
     d3.csv('data/seed.csv', function (data) {
-        // console.log(data)
-        $scope.seedData = data;
+        $scope.$apply(function () {
+            $scope.seedData = data;
+        });
     });
 
     d3.csv('data/monitoring.csv', function (data) {
         // console.log(data)
         $scope.monitoringData = data;
     });
+
+    // non-angular context에서는 $scope.$apply 하여 Model 변경을 추적해야 함.
+    d3.csv('data/oil.csv', function (data) {
+        $scope.$apply(function () {
+            $scope.oilData = data;
+        });
+    });
+
+    d3.csv('data/name.csv', function (data) {
+        $scope.$apply(function () {
+            $scope.nameData = data;
+        });
+    });
+
+    $scope.seedChanged = function (item) {
+        if (item === null) return;
+        console.log(item.name.trim());
+        var fullName = item.name.trim();
+        var arrNames = fullName.split(' ');
+
+        $scope.data.name2 = arrNames[0];
+        $scope.data.name3 = arrNames[1];
+        if (arrNames.length > 2) {
+            $scope.data.name4 = arrNames[2];
+            $scope.data.name5 = arrNames[3];
+        } else {
+            $scope.data.name4 = '';
+            $scope.data.name5 = '';
+        }
+
+        // Find Genus name
+        // Khaya
+
+        var filteredItems = $scope.nameData.filter(function (item) {
+            return item.Genus === arrNames[0];
+        })
+
+        // console.log(filteredItems[0].Family);
+        // // Family:"MELIACEAE"
+        if (filteredItems.length > 0) {
+            $scope.data.name1 = filteredItems[0].Family;
+        } else {
+            $scope.data.name1 = '';
+        }
+
+
+    }
+
 
     $scope.calculateLipCount = function () {
         // console.log("lip calculate")
@@ -139,7 +204,7 @@ app.controller('MainController', function ($scope, $http, $q, $filter, filterFil
         // console.log($scope.seedData);
 
         var searchResults = $scope.seedData.filter(function (item) {
-            return item.식물종 == searchName
+            return item.name == searchName
         })
 
         if (searchResults.length > 0) {
@@ -149,7 +214,7 @@ app.controller('MainController', function ($scope, $http, $q, $filter, filterFil
             $scope.data.Cw = searchResults[0].Cw
             $scope.data.Ch = searchResults[0].Ch
             $scope.data.Cq = searchResults[0].Cq
-            $scope.data.refDoc = searchResults[0].참고문헌
+            $scope.data.refDoc = searchResults[0].ref
         } else {
             console.log("Data not found")
             $scope.data.name1 = "NO-NAME";
@@ -174,21 +239,21 @@ app.controller('MainController', function ($scope, $http, $q, $filter, filterFil
         //
         // var logSigma = $scope.data.Ke - ( $scope.data.Cw * Math.log10($scope.data.resultWaterValue) ) - $scope.data.Ch * (-20) - $scope.data.Cq * (-20 * -20)
         // ie에서 Math.log10 오류가 있어 Math.log(f)/Math.log(10)로 대체
-        var logSigma = $scope.data.Ke - ( $scope.data.Cw * Math.log($scope.data.waterValue)/Math.log(10) ) - $scope.data.Ch * (-20) - $scope.data.Cq * (-20 * -20)
+        var logSigma = $scope.data.Ke - ( $scope.data.Cw * Math.log($scope.data.waterValue) / Math.log(10) ) - $scope.data.Ch * (-20) - $scope.data.Cq * (-20 * -20)
         // console.log("logSigma : ", logSigma)
-        $scope.data.sigmaDay = Math.round( Math.pow(10, logSigma))
+        $scope.data.sigmaDay = Math.round(Math.pow(10, logSigma))
         // console.log("$scope.data.sigmaDay : ", $scope.data.sigmaDay)
         $scope.data.sigmaYear = Math.round($scope.data.sigmaDay / 365)
         // console.log("$scope.data.sigmaYear : ", $scope.data.sigmaYear)
-        $scope.data.p90Day = Math.round( $scope.data.sigmaDay * (2.3 - 1.3))
+        $scope.data.p90Day = Math.round($scope.data.sigmaDay * (2.3 - 1.3))
         // console.log("$scope.data.p90Day : ", $scope.data.p90Day)
-        $scope.data.p90Year = Math.round($scope.data.p90Day/365)
+        $scope.data.p90Year = Math.round($scope.data.p90Day / 365)
         $scope.data.p85Day = Math.round($scope.data.sigmaDay * (2.3 - 1.0))
         // console.log("$scope.data.p85Day : ", $scope.data.p85Day)
-        $scope.data.p85Year = Math.round($scope.data.p85Day/365)
+        $scope.data.p85Year = Math.round($scope.data.p85Day / 365)
         $scope.data.p80Day = Math.round($scope.data.sigmaDay * (2.3 - 0.8))
         // console.log("$scope.data.p80Day : ", $scope.data.p80Day)
-        $scope.data.p80Year = Math.round($scope.data.p80Day/365)
+        $scope.data.p80Year = Math.round($scope.data.p80Day / 365)
         // console.log($scope.data.inventoryDate)
         $scope.data.p90_P85 = addDays($scope.data.inventoryDate, $scope.data.p90Day) + " ~ " + addDays($scope.data.inventoryDate, $scope.data.p85Day)
         // console.log("$scope.data.p90_P85 : ", $scope.data.p90_P85)
@@ -198,32 +263,41 @@ app.controller('MainController', function ($scope, $http, $q, $filter, filterFil
         // console.log("$scope.data.monitoringDays : ", $scope.data.monitoringDays)
         // console.log("$scope.data.lipCount : ", $scope.data.lipCount)
 
-        for(var i = $scope.monitoringData.length - 1; i >= 0; i--) {
+        for (var i = $scope.monitoringData.length - 1; i >= 0; i--) {
             // console.log("입고립수 : ", $scope.monitoringData[i].입고립)
             // console.log(i, $scope.monitoringData[i].입고립수, Number($scope.monitoringData[i].입고립수), $scope.data.lipCount)
-            if (Number($scope.monitoringData[i].입고립수) <= $scope.data.lipCount) {
+            if (Number($scope.monitoringData[i].inventoryLipCount) <= $scope.data.lipCount) {
                 // console.log("==== founded ", $scope.monitoringData[i].입고립수)
                 $scope.selectedMonitoringData = $scope.monitoringData[i];
                 break
             }
         }
 
-        $scope.data.testLipCount = $scope.selectedMonitoringData.모니터링사용립수;
-        $scope.data.monitoringCondition = $scope.selectedMonitoringData.발아검정립수 + '립 ' + $scope.selectedMonitoringData.반복수 + '반복 ' + $scope.selectedMonitoringData.모니터링횟수 + '회';
+        // 입고립수 inventoryLipCount
+        // 모니터링사용립수 monitoringUseLipCount
+        // 발아검정립수 sproutTestLipCount
+        // 반복수 : repeatCount
+        // 모니터링횟수 : monitoringCount
 
-        var interval = $scope.data.monitoringDay / ($scope.selectedMonitoringData.모니터링횟수 - 1 );
+        $scope.data.testLipCount = $scope.selectedMonitoringData.monitoringUseLipCount;
+        $scope.data.monitoringCondition = $scope.selectedMonitoringData.sproutTestLipCount + '립 ' + $scope.selectedMonitoringData.repeatCount + '반복 ' + $scope.selectedMonitoringData.monitoringCount + '회';
+
+        var interval = $scope.data.monitoringDay / ($scope.selectedMonitoringData.monitoringCount - 1 );
         console.log('$scope.data.p90Day : ', $scope.data.p90Day)
         var baseDate = addDays($scope.data.inventoryDate, $scope.data.p90Day)
         // console.log(baseDate)
         // baseDate = "2615.03.15"
 
+        $scope.data.monitoringDateList = []
+        $scope.data.monitoringDateList.push( { date : baseDate, power : 99, percent : 99 } )
         $scope.data.monitoringDate = baseDate;
-        for (var i=0; i < $scope.selectedMonitoringData.모니터링횟수 - 1; i++) {
+        for (var i = 0; i < $scope.selectedMonitoringData.monitoringCount - 1; i++) {
             // console.log("i : ", i, addDays(baseDate, interval * (i + 1) ))
-            $scope.data.monitoringDate = $scope.data.monitoringDate + "\n" + addDays(baseDate, interval * (i + 1) )
+            $scope.data.monitoringDate = $scope.data.monitoringDate + "\n" + addDays(baseDate, interval * (i + 1))
+            $scope.data.monitoringDateList.push( { date : addDays(baseDate, interval * (i + 1)), power : 99, percent : 99 } )
         }
 
-        // $scope.save();
+        renderSeedChart();
 
     }
 
@@ -251,12 +325,158 @@ app.controller('MainController', function ($scope, $http, $q, $filter, filterFil
         //100×((1−J4÷100)×SQRT((−440×LN(1−0.15))))÷(1.1+(15÷90))÷(100+((1−J4÷100)×SQRT((−440×LN(1−0.15))))÷(1.1+(15÷90)))
         //100×   ((1−J4÷100)×SQRT((−440×LN(1−0.15))))÷(1.1+(15÷90))÷(100+((1−J4÷100)×SQRT((−440×LN(1−0.15))))÷(1.1+(15÷90)))
 
-        var Me = ((1-$scope.data.groundValue/100) * Math.sqrt(-440 * Math.log(1-$scope.data.dryHumidity/100))) / (1.1 + ($scope.data.dryTemperature/90) )
+        var Me = ((1 - $scope.data.groundValue / 100) * Math.sqrt(-440 * Math.log(1 - $scope.data.dryHumidity / 100))) / (1.1 + ($scope.data.dryTemperature / 90) )
         // console.log(Me)
-        $scope.data.resultWaterValue = Math.round( 100 * Me / ( 100 + Me) * 100) / 100
+        $scope.data.resultWaterValue = Math.round(100 * Me / ( 100 + Me) * 100) / 100;
+
+
+        $scope.oilWaters = {
+            humidity : [10, 15, 20, 40, 50, 60],
+            water : []
+        }
+
+        for (var i=0; i < $scope.oilWaters.humidity.length; i ++) {
+            var Me = ((1 - $scope.data.groundValue / 100) * Math.sqrt(-440 * Math.log(1 - ($scope.oilWaters.humidity[i]) / 100))) / (1.1 + ($scope.data.dryTemperature / 90) )
+            var waterValue = Math.round(100 * Me / ( 100 + Me) * 100) / 100;
+
+            $scope.oilWaters.water.push(waterValue);
+        }
+        console.log($scope.oilWaters);
+
+        renderOilChart();
+
+
+
     }
 
-    $scope.calculateWaterValue()
+    // $scope.calculateWaterValue()
+
+    function renderSeedChart() {
+        Highcharts.chart('container_seed', {
+
+            credits : { enabled: false},
+
+            title: {
+                text: 'Seed Viability',
+                style: {
+                    fontWeight: 'bold',
+                    fontSize: "24px"
+                }
+            },
+
+            // subtitle: {
+            //     text: 'Source: thesolarfoundation.com'
+            // },
+
+            yAxis: {
+                title: {
+                    text: 'Viability(%)'
+                }
+            },
+            legend: {
+                enabled: false,
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle'
+            },
+
+            plotOptions: {
+                series: {
+                    pointStart: 2010
+                }
+            },
+
+            series: [{
+                name: 'Installation',
+                data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
+            }],
+
+
+            exporting: {
+                filename: 'Seed_Viability' + '-' + $scope.data.name2 + '_' + $scope.data.name3  + '-' + formatDate(new Date)
+            }
+
+
+        });
+    }
+
+    function renderOilChart() {
+        Highcharts.chart('container_oil', {
+
+            chart: {
+                height: 300,
+                type: 'line'
+            },
+            credits : { enabled: false},
+            title: {
+                text: 'Seed moisture isotherm'
+            },
+
+            // subtitle: {
+            //     text: 'Source: thesolarfoundation.com'
+            // },
+
+            xAxis: {
+                title: {
+                    text: '건조 상대습도'
+                },
+                categories: $scope.oilWaters.humidity,
+                // plotBands: [{
+                //     color: '#FCFFC5',
+                //     from: 1,
+                //     to: 2
+                // }],
+                // plotLines: [{
+                //     color: '#FF0000',
+                //     width: 2,
+                //     value: 1,
+                //     label: {
+                //         text: 'default',
+                //         verticalAlign: 'middle',
+                //         textAlign: 'center',
+                //     }
+                // }]
+            },
+
+            yAxis: {
+                title: {
+                    text: '수분함량'
+                }
+            },
+            legend: {
+                enabled: false,
+                layout: 'vertical',
+                align: 'right',
+                verticalAlign: 'middle'
+            },
+
+            tooltip: {
+                // crosshairs: [true,true],
+                crosshairs: [true, {
+                    width: 1.5,
+                    dashStyle: 'solid',
+                    color: 'red'
+                }],
+            },
+
+
+            // plotOptions: {
+            //     series: {
+            //         pointStart: 2010
+            //     }
+            // },
+
+            series: [{
+                name: '상대습도',
+                data: $scope.oilWaters.water
+            }],
+
+            exporting: {
+                filename: 'Seed_moisture_isotherm' + '-' + $scope.data.name2 + '_' + $scope.data.name3  + '-' + formatDate(new Date)
+            }
+
+        });
+    }
 
     function addDays(baseDate, numberOfDaysToAdd) {
         // console.log(baseDate)
@@ -271,9 +491,18 @@ app.controller('MainController', function ($scope, $http, $q, $filter, filterFil
         var y = someDate.getFullYear();
 
         // var someFormattedDate = dd + '/'+ mm + '/'+ y;
-        var someFormattedDate = y + '.'+ mm + '.'+ dd;
+        var someFormattedDate = y + '.' + zeroPad(mm, 2) + '.' + zeroPad(dd, 2);
         // console.log(someFormattedDate)
         return someFormattedDate
+    }
+
+    function formatDate(date) {
+        return date.getFullYear() + '.' + (Number(date.getMonth())+1) + '.' + date.getDate()
+    }
+
+    function zeroPad(num, places) {
+        var zero = places - num.toString().length + 1;
+        return Array(+(zero > 0 && zero)).join("0") + num;
     }
 
     $scope.save = function () {
@@ -282,23 +511,23 @@ app.controller('MainController', function ($scope, $http, $q, $filter, filterFil
         out = out + "\n" + "-----";
         out = out + "\n" + "종정보 : " + $scope.data.name2 + " " + $scope.data.name3 + " " + $scope.data.name4 + " " + $scope.data.name5 + " (" + $scope.data.refDoc + ")";
         out = out + "\n" + "수분함량 : " + $scope.data.waterValue + ", 저장조건 : " + $scope.data.inventoryCondition;
-        out = out + "\n" + "입고일 : " + $scope.data.inventoryDate +  ", 중량 : " + $scope.data.weight + ", 1000립중 : " + $scope.data.kiloLipWeight + ", 립수 : " + $scope.data.lipCount;
+        out = out + "\n" + "입고일 : " + $scope.data.inventoryDate + ", 중량 : " + $scope.data.weight + ", 1000립중 : " + $scope.data.kiloLipWeight + ", 립수 : " + $scope.data.lipCount;
         out = out + "\n" + "입고활력 : " + $scope.data.inventoryPower
 
         out = out + "\n";
         out = out + "\n" + "지질함량에 따른 수분함량 계산";
         out = out + "\n" + "-----";
-        out = out + "\n" + "지질함량 : " + $scope.data.groundValue +  ", 건조온도 : " + $scope.data.dryTemperature + ", 건조 상대습도 : " + $scope.data.dryHumidity + ", 수분함량 : " + $scope.data.resultWaterValue;
+        out = out + "\n" + "지질함량 : " + $scope.data.groundValue + ", 건조온도 : " + $scope.data.dryTemperature + ", 건조 상대습도 : " + $scope.data.dryHumidity + ", 수분함량 : " + $scope.data.resultWaterValue;
 
         out = out + "\n";
         out = out + "\n" + "결과";
         out = out + "\n" + "-----";
         out = out + "\n" + "수명공식계수 : " + $scope.seed;
-        out = out + "\n" + "Ke : " + $scope.data.Ke +  ", Cw : " + $scope.data.Cw + ", Ch : " + $scope.data.Ch + ", Cq : " + $scope.data.Cq;
-        out = out + "\n" + "Sigma(Day) : " + $scope.data.sigmaDay +  ", P90(Day) : " + $scope.data.p90Day + ", P85(Day) : " + $scope.data.p85Day + ", P80(Day) : " + $scope.data.p80Day;
-        out = out + "\n" + "Sigma(Year) : " + $scope.data.sigmaYear +  ", P90(Year) : " + $scope.data.p90Year + ", P85(Year) : " + $scope.data.p85Year + ", P80(Year) : " + $scope.data.p80Day;
-        out = out + "\n" + "P90-P85 : " + $scope.data.p90_P85 +  ", P85-P80 : " + $scope.data.p85_P80;
-        out = out + "\n" + "모니터링 기간day : " + $scope.data.monitoringDay +  ", 권장 검정립수 : " + $scope.data.testLipCount + ", 권장 모니터링 조건 : " + $scope.data.monitoringCondition;
+        out = out + "\n" + "Ke : " + $scope.data.Ke + ", Cw : " + $scope.data.Cw + ", Ch : " + $scope.data.Ch + ", Cq : " + $scope.data.Cq;
+        out = out + "\n" + "Sigma(Day) : " + $scope.data.sigmaDay + ", P90(Day) : " + $scope.data.p90Day + ", P85(Day) : " + $scope.data.p85Day + ", P80(Day) : " + $scope.data.p80Day;
+        out = out + "\n" + "Sigma(Year) : " + $scope.data.sigmaYear + ", P90(Year) : " + $scope.data.p90Year + ", P85(Year) : " + $scope.data.p85Year + ", P80(Year) : " + $scope.data.p80Day;
+        out = out + "\n" + "P90-P85 : " + $scope.data.p90_P85 + ", P85-P80 : " + $scope.data.p85_P80;
+        out = out + "\n" + "모니터링 기간day : " + $scope.data.monitoringDay + ", 권장 검정립수 : " + $scope.data.testLipCount + ", 권장 모니터링 조건 : " + $scope.data.monitoringCondition;
         out = out + "\n" + "권장 모니터링일 : ";
         out = out + '\n- ' + $scope.data.monitoringDate.split('\n').join('\n- ');
 
@@ -316,7 +545,7 @@ app.controller('MainController', function ($scope, $http, $q, $filter, filterFil
         console.log(csvData);
         // csvData = csvData + 'title,"지질함량에 따른 수분함량 계산"';
 
-        var blob = new Blob([csvData], { type: "text/csv;charset=utf-8" });
+        var blob = new Blob([csvData], {type: "text/csv;charset=utf-8"});
         saveAs(blob, "myFile.csv");
 
         // var csvData = '';
@@ -331,4 +560,4 @@ app.controller('MainController', function ($scope, $http, $q, $filter, filterFil
 
 
     }
-});
+})
